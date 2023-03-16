e5f7796bd3c72e4da8aff017cb348bdc186a93d0
Ryzxxl <rakshitraina1234@gmail.com>
2023-03-16 11:09:27 +0530

    added data

--- setup.py
+++ setup.py
@@ -17,11 +17,10 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='bbc-classification',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Rakshit, Prikshit',
+author_email='rakshitraina1234@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
-
 )
