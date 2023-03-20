b85ab4b6376d6dfca70f0ba4fac948d9e5188db4
Ryzxxl <rakshitraina1234@gmail.com>
2023-03-20 15:14:12 +0530

    prediction prob

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import streamlit as st
 import sys
 import string
 from src.exception import CustomException
+import pandas as pd
+import altair as alt
+
 
 html_temp = """
 <div style ="background-color:#51E1ED;padding:13px">
@@ -18,27 +21,48 @@ def main():
     try:
             raw_text = st.text_area("Type your Text here .....",key="text")
             no_punct = raw_text.translate(str.maketrans("", "", string.punctuation))
-            col1, col2, col3 = st.columns(3)
+            col1, col2 = st.columns(2)
 
             def clear_text():
                 st.session_state["text"] = ""
+
                 
                 
             if st.button("FIND"):
-                if len(no_punct.split())>=5:
 
-                    PredictPipeline=predict_pipeline.PredictPipeline(raw_text)
-                    pred,probability=PredictPipeline.predict()
+                with col1:
+                     st.success("Original Text")
+                     st.write(raw_text)
+                     st.success("Prediction Probability")
+                     if len(no_punct.split())>=5:
+                          PredictPipeline=predict_pipeline.PredictPipeline(raw_text)
+                          pred,probability=PredictPipeline.predict()
+
+                          proba_df = pd.DataFrame(probability)
+                        #   st.write(proba_df.T, ascending = False)
+                          proba_df_clean = proba_df.T.reset_index()
+                          proba_df_clean.columns = ["Category", "Probability"]
+                          st.write(proba_df_clean, ascending = False)
+
+                        #   col2.write(pred)
+                        #   col2.write(probability)
+                    
+                          st.button("REST", on_click=clear_text)
+                     else:
+                        col2.write("Provide more information ")
+                    
+                        st.button("REST", on_click=clear_text)
+
+                with col2:
+                          
+                          st.success("Prediction")
+                          st.write(pred) 
+                          st.success("Probability Graph")
+                          fig = alt.Chart(proba_df_clean).mark_bar().encode(x='Category',y='Probability')
+                          st.altair_chart(fig,use_container_width=True)
 
 
-                    col2.write(pred)
-                    col2.write(probability)
-                
-                    st.button("REST", on_click=clear_text)
-                else:
-                    col2.write("Provide more information ")
-                
-                    st.button("REST", on_click=clear_text)
+                    
                 
                      
 
