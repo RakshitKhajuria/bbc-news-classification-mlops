771d226c24b7c02e0696fb6fc7b5c3afdccc9620
Ryzxxl <rakshitraina1234@gmail.com>
2023-03-21 18:43:01 +0530

    preprocess changed

--- src/TextProcessing.py
+++ src/TextProcessing.py
@@ -1,11 +1,12 @@
-from keras.preprocessing.text import text_to_word_sequence
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
 from src.exception import CustomException
+from nltk.tokenize import word_tokenize
 import sys
+import string
 # nltk.download('stopwords')
 # nltk.download('punkt')
 # nltk.download('averaged_perceptron_tagger')
@@ -14,31 +15,32 @@ import sys
 
 def preprocessDataset(train_text):
     try:
-       
+
         #word tokenization using text-to-word-sequence
         train_text= str(train_text)
-        tokenized_train_set = text_to_word_sequence(train_text,filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',lower=True,split=" ")
-            
+        no_punct=train_text.translate(str.maketrans("", "", string.punctuation)).lower()
+        tokenized_train_set = word_tokenize(no_punct) 
+
         #stop word removal
         stop_words = set(stopwords.words('english'))
         stopwordremove = [i for i in tokenized_train_set if not i in stop_words]
-            
-        
+
+
         #join words into sentence
         stopwordremove_text = ' '.join(stopwordremove)
-            
-            
+
+
         #remove numbers
         numberremove_text = ''.join(c for c in stopwordremove_text if not c.isdigit())
-        
-            
+
+
         #--Stemming--
         stemmer= PorterStemmer()
 
         stem_input=nltk.word_tokenize(numberremove_text)
         stem_text=' '.join([stemmer.stem(word) for word in stem_input])
-            
-            
+
+
         lemmatizer = WordNetLemmatizer()
 
         def get_wordnet_pos(word):
@@ -53,7 +55,7 @@ def preprocessDataset(train_text):
 
         lem_input = nltk.word_tokenize(stem_text)
         lem_text= ' '.join([lemmatizer.lemmatize(w, get_wordnet_pos(w)) for w in lem_input])
-            
+
         return lem_text
     except Exception as e:
         raise CustomException(e,sys)
